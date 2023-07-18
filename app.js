906e476ceb7853379b6b41b9467d656ded78d57b
HotG8119 <k508060@gmail.com>
2023-07-18 19:35:53 +0800

    refactoring: Separate mongoose

--- app.js
+++ app.js
@@ -3,33 +3,15 @@ const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+const routes = require("./routes");
+
+require("./config/mongoose");
 
 const app = express();
-const port = 3000;
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-
-const routes = require("./routes");
+const port = process.env.PORT || 3000;
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
-
-const db = mongoose.connection;
-
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
 
 // setting static files
 app.use(express.static("public"), express.urlencoded({ extended: true }));

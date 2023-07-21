a9915e32a2417c1de1dd257bd6b6aa98fcdfb34b
HotG8119 <k508060@gmail.com>
2023-07-21 13:19:00 +0800

    fix: Correct error in add route

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -10,9 +10,9 @@ router.get("/add", (req, res) => {
 
 router.post("/add", (req, res) => {
   const userId = req.user._id;
-  const { name, amount, category } = req.body;
-
-  return Record.create({ name, amount, category, userId })
+  const { name, date, amount, category } = req.body;
+  console.log(req.body);
+  return Record.create({ name, date, amount, category, userId })
     .then(() => res.redirect("/"))
     .catch(err => console.log(err));
 });

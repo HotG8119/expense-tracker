f5e936e16ab380081cdbcf0e0e9e71121b98b992
HotG8119 <k508060@gmail.com>
2023-07-21 23:33:08 +0800

    refactor: use for loop to create SEED_USER

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -5,7 +5,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 const Record = require('../record')
 const User = require('../user')
-const Category = require('../category')
 const db = require('../../config/mongoose')
 
 const SEED_RECORDS = require('./date/records.json')
@@ -16,31 +15,30 @@ db.on('error', () => {
 })
 
 db.once('open', () => {
-  return Promise.all(
-    SEED_USERS.map(SEED_USER => {
-      bcrypt
-        .genSalt(10)
-        .then(salt => bcrypt.hash(SEED_USER.password, salt))
-        .then(hash =>
-          User.create({
-            name: SEED_USER.name,
-            email: SEED_USER.email,
-            password: hash
-          })
-        )
-        .then(user => {
-          const userId = user._id
+  for (const [index, SEED_USER] of SEED_USERS.entries()) {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(SEED_USER.password, salt))
+      .then(hash =>
+        User.create({
+          name: SEED_USER.name,
+          email: SEED_USER.email,
+          password: hash
+        })
+      )
+      .then(user => {
+        const userId = user._id
 
-          const record = SEED_USER.recordId.map(index => {
-            SEED_RECORDS[index].userId = userId
-            return SEED_RECORDS[index]
-          })
-          return Record.create(record)
+        const record = SEED_USER.recordId.map(index => {
+          SEED_RECORDS[index].userId = userId
+          return SEED_RECORDS[index]
         })
-        .then(() => {
-          console.log('done')
-          process.exit()
-        })
-    })
-  )
+        console.log(`第${index + 1}筆使用者資料建立完成`)
+        return Record.create(record)
+      })
+      .then(() => {
+        console.log('done')
+        process.exit()
+      })
+  }
 })
